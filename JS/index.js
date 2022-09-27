bed0715f47a243594801bc68856ee967edf1189e
Grace Abike <mara234216@gmail.com>
2022-09-27 22:42:48 +0200

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,48 @@
-const libraryBooksEl = document.querySelector('.library-books');
+const listEl = document.querySelector('.list');
+const addnewEl = document.querySelector('.addnew');
+const contactEl = document.querySelector('.contact');
+const date = document.querySelector('.date');
+const booksListEl = document.querySelector('.awesome-book');
+const libraryBooksEl = document.querySelector('.list-books');
 const addElBtn = document.querySelector('.btn-add');
+const contactInformEl = document.querySelector('.contact-inform');
+const form = document.querySelector('form');
 const titleEl = document.getElementById('title');
 const authorEl = document.getElementById('author');
-const errorMsgEl = document.querySelector('.errorMsg');
-const form = document.querySelector('form');
+const alertMessageEl = document.querySelector('.alertMessage');
+setInterval(() => {
+  date.innerHTML = new Date();
+}, 1000);
+
+const toggleWindow = () => {
+  listEl.addEventListener('click', () => {
+    form.classList.remove('show');
+    contactInformEl.classList.remove('show');
+    booksListEl.classList.remove('hide');
+    listEl.style.color = 'blue';
+    addnewEl.style.color = 'black';
+    contactEl.style.color = 'black';
+  });
+
+  addnewEl.addEventListener('click', () => {
+    form.classList.add('show');
+    contactInformEl.classList.remove('show');
+    booksListEl.classList.add('hide');
+    listEl.style.color = 'black';
+    addnewEl.style.color = 'blue';
+    contactEl.style.color = 'black';
+  });
+
+  contactEl.addEventListener('click', () => {
+    form.classList.remove('show');
+    contactInformEl.classList.add('show');
+    booksListEl.classList.add('hide');
+    listEl.style.color = 'black';
+    addnewEl.style.color = 'black';
+    contactEl.style.color = 'blue';
+  });
+};
+
 const bookList = JSON.parse(localStorage.getItem('bookList')) || [];
 
 class Library {
@@ -22,17 +61,22 @@ class Library {
           author,
         };
         bookList.push(newBook);
-        localStorage.setItem('bookList', JSON.stringify(bookList));
+        localStorage.setItem('book-List', JSON.stringify(bookList));
         this.renderBooks();
         form.reset();
-        errorMsgEl.innerHTML = '';
+        alertMessageEl.innerHTML = 'Book added successfully, <br> Check list.';
+        alertMessageEl.style.color = 'blue';
       } else {
-        errorMsgEl.innerHTML = 'Input something';
+        alertMessageEl.innerHTML = 'Input something';
+        alertMessageEl.style.color = 'red';
       }
+      setTimeout(() => {
+        alertMessageEl.innerHTML = '';
+      }, 1500);
     });
   }
 
-  renderBooks() {
+  awesomeBooks() {
     if (!bookList.length) {
       libraryBooksEl.innerHTML = 'No books added';
     } else {
@@ -53,15 +97,15 @@ class Library {
         item.addEventListener('click', (e) => {
           bookList.splice(e.target.id, 1);
           localStorage.setItem('bookList', JSON.stringify(bookList));
-          this.renderBooks();
+          this.awesomeBooks();
         });
       });
     };
     removeBook();
   }
 }
-
 const awesomeBooks = new Library();
 
 awesomeBooks.addBook();
-awesomeBooks.renderBooks();
+awesomeBooks.awesomeBooks();
+toggleWindow();
